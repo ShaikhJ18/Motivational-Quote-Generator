a30546186d94f184fcb2da37a5f034371d24c28a
ShaikhJ18 <J.shk1980@gmail.com>
2024-02-21 16:24:36 -0500

    adjusted the quotes added a counter and adjusted some css values

--- logic.js
+++ logic.js
@@ -1,13 +1,16 @@
 const quote = document.querySelector(".quote-content");
-const author = document.querySelector(".author")
-const btn = document.querySelector(".Generate")
-
+const author = document.querySelector(".author");
+const btn = document.querySelector(".Generate");
+const numOfQuotes = document.querySelector(".Number-Of-Quotes");
+let numOfQuotesCreated = 0;
 function getQuote(){  
-    fetch("https://quotable.io/random")
+    fetch("https://quotable.io/random?maxLength=50")
     .then(res => res.json())
     .then(data =>{
         quote.innerHTML = `"${data.content}"`;
         author.innerHTML = data.author;
     })
+    numOfQuotesCreated++;
+    numOfQuotes.innerHTML = numOfQuotesCreated;
 }
 btn.addEventListener("click",getQuote)
